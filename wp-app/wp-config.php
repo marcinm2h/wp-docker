2acf11fc049d14adbaaec2d0fd7f903c4204c829
Marcin <marcio.moch@gmail.com>
2019-12-03 00:45:22 +0100

    Added php config

--- wp-app/wp-config.php
+++ wp-app/wp-config.php
@@ -2,12 +2,6 @@
 // https://stackoverflow.com/questions/640409/can-i-install-update-wordpress-plugins-without-providing-ftp-access
 define('FS_METHOD', 'direct');
 
-// https://help.servmask.com/2018/10/27/how-to-increase-maximum-upload-file-size-in-wordpress/
-@ini_set('upload_max_filesize' , '128M');
-@ini_set('post_max_size', '128M');
-@ini_set('memory_limit', '256M');
-@ini_set('max_execution_time', '300');
-@ini_set('max_input_time', '300');
 /**
  * The base configuration for WordPress
  *
