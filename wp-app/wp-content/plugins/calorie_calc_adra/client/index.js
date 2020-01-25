038bc6fb6b40de2f22640c23e5b3f76794be1ee6
Marcin Moch <marcio.moch@gmail.com>
2020-01-25 18:47:38 +0100

    Editing / removing items

--- wp-app/wp-content/plugins/calorie_calc_adra/client/index.js
+++ wp-app/wp-content/plugins/calorie_calc_adra/client/index.js
@@ -10,11 +10,18 @@ class AddButton extends Component {
 
 
 class PlanItem extends Component {
-  render({ exercise, kcal, onClick, ...props }) {
+  render({ exercise, kcal, minutes, onRemove, onMinutesChange, ...props }) {
     return html`
       <li>
-        <input />
-        <button>usuń</button>
+        <label>
+          ${exercise} <input
+            type="number"
+            min="0"
+            onInput=${e => onMinutesChange(Number(e.target.value))}
+          /> min
+        </label>
+        ${ minutes ? minutes * kcal : undefined} kcal
+        <button onClick=${e => onRemove()}>usuń</button>
       </li>
       `;
   }
@@ -23,31 +30,81 @@ class PlanItem extends Component {
 // TODO: pobieranie przez a.download
 
 class CalorieCalculator extends Component {
-  addToPlan(exercise) {
-    const { plan = [] } = this.state;
-    this.setState({ plan: plan.concat(exercise) });
+  state = {
+    uuid: 0,
+    plan: []
+  }
+
+  addItemToPlan(exercise) {
+    this.setState({ plan: this.state.plan.concat({ ...exercise, uuid: this.state.uuid++ }) });
+  }
+
+  updateItem(uuid, newState) {
+    this.setState({
+      plan: this.state.plan.map(item => {
+        if (item.uuid !== uuid) {
+          return item;
+        }
+        return {
+          ...item,
+          ...newState
+        }
+      })
+    });
+  }
+
+  removeItem(uuid) {
+    this.setState({
+      plan: this.state.plan.filter(item => item.uuid !== uuid)
+    })
+  }
+
+  generateSummary() {
+    return this.state.plan.reduce((acc, item) => ({
+      minutes: acc.minutes + item.minutes,
+      kcal: acc.kcal + item.kcal * item.minutes
+    }), { minutes: 0, kcal: 0 })
   }
 
   render({ exercises = [] }, { plan = [] }) {
+    const summary = this.generateSummary();
+
     return html`
       <div>
       <h3>Dodaj ćwiczenie</h3>
       <div>
         ${exercises.map(exercise => html`
-          <${AddButton} ...${exercise} onClick=${e => this.addToPlan(exercise)} />
+          <${AddButton}
+            ...${exercise}
+            onClick=${e => this.addItemToPlan({ ...exercise, minutes: 0 })}
+          />
         `)}
       </div>
       <div>
         <ul>
-          ${plan.map(item => html`<${PlanItem} ...${item} />`)}
+          ${plan.map(item => html`
+            <${PlanItem}
+              ...${item}
+              onMinutesChange=${minutes => this.updateItem(item.uuid, { minutes })}
+              onRemove=${() => this.removeItem(item.uuid)}
+            />
+          `)}
         </ul>
       </div>
       <div>
       ${plan.length > 0 ? (
         html`
+          <hr />
+          <h3>Podsumowanie</h3>
+          <label>
+            Minut łączne: <input disabled value=${summary.minutes} />
+          </label>
+          <label>
+            Kalorii łączne: <input disabled value=${summary.kcal} />
+          </label>
           <button>Eskportuj do CSV</button>
         `
-      ): null}
+      ) : null}
       </div>
       </div>
     `;
